4b112784375bd924185d22a1280d7f63fbbeb4eb
erno <js1239080@gmail.com>
2022-10-18 16:26:14 +0300

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,31 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'petyr-plet-plete-prez-tri-pryta-preplita')
   end
+
+  def decode_token
+    auth_header = request.headers['Autorization']
+    if auth_header
+      token = auth_header.split(' ').last if auth_header
+      
+      begin
+        JWT.decode(token, 'petyr-plet-plete-prez-tri-pryta-preplita',
+          true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+  
+      end
+    end
+  end
+
+  def authorized_user
+    if decode_token
+      user_id = decode_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def authorize
+    render json: { message: 'Please log in' },
+      status: :unauthorized unless authorized_user
+  end
 end
