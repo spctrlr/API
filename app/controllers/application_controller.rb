7d864f838f277c55da63075c3347cafc611136d0
erno <js1239080@gmail.com>
2022-10-19 23:02:56 +0300

    refactoring jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::API
+
   def encode_token(payload)
     JWT.encode(payload, 'petyr-plet-plete-prez-tri-pryta-preplita')
   end
 
+  def auth_header
+    request.headers['Autorization']
+  end
+
   def decode_token
-    auth_header = request.headers['Autorization']
     if auth_header
       token = auth_header.split(' ').last if auth_header
       
@@ -18,15 +22,20 @@ class ApplicationController < ActionController::API
     end
   end
 
-  def authorized_user
+  def current_user
     if decode_token
       user_id = decode_token[0]['user_id']
       @user = User.find_by(id: user_id)
     end
   end
 
-  def authorize
-    render json: { message: 'Please log in' },
-      status: :unauthorized unless authorized_user
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    unless logged_in?
+      render json: { message: 'Please log in' }, status: :unauthorized
+    end
   end
 end
