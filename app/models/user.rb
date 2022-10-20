61d2f89fb8409cf735128e6b69b3e18f3ba56635
erno <js1239080@gmail.com>
2022-10-20 12:02:50 +0300

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { in: 0..20 }
+  validates :username, presence: true, uniqueness: true, length: { in: 1..20 }
   validates :email, confirmation: true, presence: true, uniqueness: true
   validates :password, confirmation: true, presence: true, length: { in: 6..20 }
   validates :password_confirmation, confirmation: true, presence: true, length: { in: 6..20 }
