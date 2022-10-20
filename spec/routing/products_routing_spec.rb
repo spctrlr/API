d86d04f8ac1e2891520ecbd33aae35fe45e4a01c
erno <js1239080@gmail.com>
2022-10-20 20:20:59 +0300

    update and cleanup routing specs for users/products

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -3,28 +3,34 @@ require "rails_helper"
 RSpec.describe ProductsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/products").to route_to("products#index")
+      expect(get: "/brands/1/products").to route_to(:controller => "products",
+        :brand_id => "1", :action => "index")
     end
 
     it "routes to #show" do
-      expect(get: "/products/1").to route_to("products#show", id: "1")
+      expect(get: "/brands/1/products/1").to route_to(:controller => "brands", 
+        :brand_id => "1", :controller => "products", :action => "show", :id => "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/products").to route_to("products#create")
+      expect(post: "/brands/1/products").to route_to(:controller => "brands", 
+        :brand_id => "1", :controller => "products", :action => "create")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/products/1").to route_to("products#update", id: "1")
+      expect(put: "/brands/1/products/1").to route_to(:controller => "brands", 
+        :brand_id => "1", :controller => "products", :action => "update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/products/1").to route_to("products#update", id: "1")
+      expect(patch: "/brands/1/products/1").to route_to(:controller => "brands", 
+        :brand_id => "1", :controller => "products", :action => "update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/products/1").to route_to("products#destroy", id: "1")
+      expect(delete: "/brands/1/products/1").to route_to(:controller => "brands", 
+        :brand_id => "1", :controller => "products", :action => "destroy", :id => "1")
     end
   end
 end
