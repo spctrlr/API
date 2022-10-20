23ed3f45cdaa9bb4fed5b95264c196f384447665
erno <js1239080@gmail.com>
2022-10-20 12:00:33 +0300

    routing spec for vouchers

--- spec/routing/vouchers_routing_spec.rb
+++ spec/routing/vouchers_routing_spec.rb
@@ -2,29 +2,35 @@ require "rails_helper"
 
 RSpec.describe VouchersController, type: :routing do
   describe "routing" do
+
     it "routes to #index" do
-      expect(get: "/vouchers").to route_to("vouchers#index")
+      expect(get: "/users/1/vouchers").to route_to(:controller => "vouchers",
+        :user_id => "1", :action => "index")
     end
 
     it "routes to #show" do
-      expect(get: "/vouchers/1").to route_to("vouchers#show", id: "1")
+      expect(get: "/users/1/vouchers/1").to route_to(:controller => "users", 
+        :user_id => "1", :controller => "vouchers", :action => "show", :id => "1")
     end
 
-
     it "routes to #create" do
-      expect(post: "/vouchers").to route_to("vouchers#create")
+      expect(post: "/users/1/vouchers").to route_to(:controller => "vouchers",
+        :user_id => "1", :action => "create")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/vouchers/1").to route_to("vouchers#update", id: "1")
+      expect(put: "/users/1/vouchers/1").to route_to(:controller => "users", 
+        :user_id => "1", :controller => "vouchers", :action => "update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/vouchers/1").to route_to("vouchers#update", id: "1")
+      expect(patch: "/users/1/vouchers/1").to route_to(:controller => "users", 
+        :user_id => "1", :controller => "vouchers", :action => "update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/vouchers/1").to route_to("vouchers#destroy", id: "1")
+      expect(delete: "/users/1/vouchers/1").to route_to(:controller => "users", 
+        :user_id => "1", :controller => "vouchers", :action => "destroy", :id => "1")
     end
   end
 end
