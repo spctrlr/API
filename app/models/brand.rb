98679f7771bd354fa095af78412b3e3c163f607a
erno <js1239080@gmail.com>
2022-10-20 13:08:11 +0300

    update brand model

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,7 @@
 class Brand < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { in: 1..20 }
+  validates :address, confirmation: true, presence: true, length: { in: 6..50 }
+  validates :email, confirmation: true, presence: true, uniqueness: true
+
+  # belongs_to :admin
 end
