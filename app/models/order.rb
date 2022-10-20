2a34e7d10c0f5ad17ae663ed9460b31e2a0cc548
erno <js1239080@gmail.com>
2022-10-21 01:59:20 +0300

    update quantity validation in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
 
   validates :final_amount, presence: true, numericality: { greater_than: 0 }
   validates :currency, presence: true, length: { maximum: 5 }
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }, length: { maximum: 2 }
 
   belongs_to :voucher
   belongs_to :product
